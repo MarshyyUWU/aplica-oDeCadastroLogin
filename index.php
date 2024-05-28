34560e845c44cfdb344aeaf552ac3728419db102
MarshyyUWU <marshmelloweeee@gmail.com>
2024-05-28 11:33:54 -0300

    adicionei html basico nas paginas, com sink em cad

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-uwu
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <a href="index.php">Inicio</a>
+    <a href="cad.php">Cadastro</a>
+    <a href="log.php">Login</a>
+</body>
+</html>
---
+++ log.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="index.php">Inicio</a>
+    <a href="cad.php">Cadastro</a>
+    <a href="log.php">Login</a>
+</body>
+</html>
